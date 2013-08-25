8d6c0a7f75a5e8a8c4e5bca3ae27897bddc049aa
inframatic <isteffy@hotmail.com>
2013-08-25 17:17:22 -0400

    .

--- CRM.rb
+++ CRM.rb
@@ -3,6 +3,10 @@ require("./Database.rb")
 
 class CRM
 
+  def initialize(name)
+    @name = name
+  end 
+
   def main_menu
     print_main_menu
     user_selected = gets.chomp.to_i
@@ -36,7 +40,7 @@ class CRM
     last_name = gets.downcase.capitalize.chomp
     print "Enter Email Address: "
     email = gets.downcase.chomp
-    print "Enter a Note: "
+    print "Enter a Note:"
     note = gets.downcase.chomp
     contact = Contact.new(first_name, last_name, email, note)
     Database.add_contact(contact)
@@ -48,7 +52,7 @@ class CRM
 
   def modify_existing_contact
     puts "\e[H\e[2J"
-    puts "Please type your ID"
+    puts "Please type the ID to be modified"
     contact_to_be_modified = gets.chomp.to_i
     if contact_to_be_modified == ""
       puts "Unknown ID"
@@ -58,27 +62,110 @@ class CRM
       confirmation_selection = gets.chomp.downcase
     end 
 
+    if confirmation_selection == "y"
+      puts "What would you like to change?"
+      puts "[1] first name"
+      puts "[2] last name"
+      puts "[3] email address"
+      puts "[4] note"
+      puts "[5] id "
+      attribute_selection_input = gets.chomp
+    else 
+      main_menu
+    end
+
+      case attribute_selection_input
+        when "1" then  
+                puts "Please change the first name"
+                first_name = gets.chomp
+                Database.modify_first_name(first_name, contact_to_be_modified)
+                main_menu
+
+        when "2" then
+                puts "Please change the second name"
+                last_name = gets.chomp
+                Database.modify_last_name(last_name, contact_to_be_modified)
+                main_menu
+
+        when "3" then  
+                puts "Please change the email address"
+                email = gets.chomp
+                Database.modify_email(email, contact_to_be_modified)
+
+        when "4" then
+                puts "Please change the note"
+                note = gets.chomp
+                Database.modify_note(note, contact_to_be_modified)
+        when "5" then 
+                puts "Please change the ID"
+                new_id = gets.chomp
+                Database.modify_id(new_id, contact_to_be_modified)          
+        else 
+            puts "Whachu talkin bout Willis, we're headin back to the start"
+            main_menu
+      end
   end
 
   def delete_a_contact
+    puts "Are you sure you would like to delete the contact?"
+    delete_contact_verification = gets.chomp
 
-  end
-
+    if delete_contact_verification == "y"
+      puts "Enter the ID"
+      delete_ID = gets.chomp.to_i
+      Database.delete_a_contact(delete_ID)
+      puts "ID deleted"
+      main_menu
+    else
+      main_menu
+    end 
+   end
+    
   def display_all_the_contacts
-
+      all_contacts = Database.display_all_the_contacts
+      all_contacts
+      main_menu
   end
 
   def display_an_attribute
+    puts "What would you like to see?"
+    puts "[1] name"
+    puts "[2] email"
+    puts "[3] note"
+    puts "[4] id" 
 
+    attribute_to_be_displayed = gets.chomp
+
+    case attribute_to_be_displayed
+      when "1" then
+                  all_name = Database.display_all_name
+                  all_name
+                  main_menu
+      when "2" then
+                  all_email = Database.display_all_email
+                  all_email
+                  main_menu
+      when "3" then
+                  all_note = Database.display_all_note
+                  all_note
+                  main_menu               
+      when "4" then
+                  all_id = Database.display_all_id
+                  all_id
+                  main_menu                            
+      else main_menu
+
+    end
   end
 
   def exit
-
+    puts "You are now exiting the program"
+    abort
   end
 
 end
 
-a_crm_app = CRM.new
+a_crm_app = CRM.new("crm")
 a_crm_app.main_menu
 
 
--- Database.rb
+++ Database.rb
@@ -19,17 +19,63 @@ class Database
 	
 	def self.modify_first_name(first_name, contact_to_be_modified)
 		#puts I'm in modify email method
-		contacts.each {|x| x.first_name = first_name if x.id == contact_to_be_modified}
+		@contacts.each {|x| x.first_name = first_name if x.id == contact_to_be_modified}
 	end	
 
 	def self.modify_last_name(last_name, contact_to_be_modified)
 
-		contacts.each {|x| x.last_name = last_name if x.id == contact_to_be_modified}
+		@contacts.each {|x| x.last_name = last_name if x.id == contact_to_be_modified}
 	end	
 
-@contacts.each {|x| x.id = new_id if x.id == contact_to_be_modified}
+	def self.modify_email(email, contact_to_be_modified)
+
+		@contacts.each {|x| x.email = email if x.id == contact_to_be_modified}
+	end	
+
+	def self.modify_note(note, contact_to_be_modified)
+
+		@contacts.each {|x| x.note = note if x.id == contact_to_be_modified}
 	end
+		
+	def self.modify_id(id, contact_to_be_modified)
 
+		@contacts.each {|x| x.id = new_id if x.id == contact_to_be_modified}
+
+	end 
+
+	def self.display_all_contacts
+    	@contacts.each do |contact| 
+      		puts ""
+      		puts ""
+      		puts ""
+      		puts "[id] : #{contact.id}"
+      		puts "[name] :  #{contact.first_name} #{contact.last_name}" 
+     		puts "[email] : #{contact.email}"
+      		puts "[note] : #{contact.note}"
+      		puts ""
+      		puts ""
+      		puts ""
+    	end  
+  	end
+
+  def self.display_all_name
+    @contacts.each {|contact| puts "[name] :  #{contact.first_name} #{contact.last_name}" } 
+  end
+
+  def self.display_all_email
+    @contacts.each {|contact| puts "[email] :  #{contact.email}"  } 
+  end
+
+  def self.display_all_note
+    @contacts.each {|contact| puts "[note] :  #{contact.note}" } 
+  end
+
+  def self.display_all_id
+    @contacts.each {|contact| puts "[id] :  #{contact.id}" } 
+  end
+
+  def self.delete_a_contact(delete_ID)
+
+    @contacts.delete_if {|contact| contact.id == delete_ID}
+  end
 end
-
-
